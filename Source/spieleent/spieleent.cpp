2a226c4d59724c54370d89ba75c3d27b811f6fb2
Podiebradju <xxflappaxx@gmail.com>
2022-03-19 15:21:38 +0100

    init unreal

---
+++ Source/spieleent/spieleent.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "spieleent.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, spieleent, "spieleent" );
+ 
---
+++ Source/spieleent/spieleentGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "spieleentGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AspieleentGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AspieleentGameMode();
+};
+
+
+
